35f14ddd838153cf0dace93a24f192ffffdd82f1
mndev2019 <muskanvision2019@gmail.com>
2024-07-05 14:33:29 +0530

    mm

--- src/Home/Actionbox.jsx
+++ src/Home/Actionbox.jsx
@@ -1,7 +1,10 @@
 //import React from 'react'
-
+import { useState } from "react"
+import Modelcomponent from "./Modelcomponent";
 
 const Actionbox = () => {
+    const [showMyModal,setShowMyModal] = useState(false)
+    const handleOnClose =() => setShowMyModal(false)
     return (
         <>
             <section className='lg:p-10 p-5 actionbox  object-cover w-full'>
@@ -12,7 +15,7 @@ const Actionbox = () => {
                                 <p className="text-white font-light lg:text-[1.5rem] md:text-[1.3rem] text-[10px] pb-5">DON’T HESTITATE TO CONTACT US</p>
                                 <h1 className="text-white font-bold opacity-3 lg:text-[3rem] md:[2rem] text-[1rem]">MAKE AN APPOINTMENT NOW <span className="block font-light lg:text-[3rem] md:text-[2rem] text-[1rem] pt-5">8595076589</span></h1>
                                 <div className="w-full pt-5">
-                                    <button className="text-white lg:text-[1.2rem] md:text-[1rem] text-[10px]  bg-[#0c3493] lg:px-4 lg:py-2 px-2 py-1  lg:rounded-lg rounded-md
+                                    <button onClick={()=> setShowMyModal(true)} className="text-white lg:text-[1.2rem] md:text-[1rem] text-[10px]  bg-[#0c3493] lg:px-4 lg:py-2 px-2 py-1  lg:rounded-lg rounded-md
                      ">CONTACT US</button>
                                 </div>
                             </div>
@@ -20,6 +23,7 @@ const Actionbox = () => {
                     </div>
                 </div>
             </section>
+            <Modelcomponent onClose={handleOnClose} visible={showMyModal}/>
         </>
     )
 }
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -4,9 +4,9 @@ import "slick-carousel/slick/slick-theme.css";
 import banner1 from '../assets/young-man-working-office-with-colleague.jpg'
 import banner3 from '../../src/assets/young-business-typing-netbook-keyboard-listening-music.jpg'
 import banner4 from '../assets/young-fashion-smiling-hipster-man-city-cafe-lunch-time-with-notebook-suit.jpg'
-
+import logo from '../../src/assets/logo.png'
 import Slider from "react-slick";
-import { LeftOutlined, RightOutlined, SearchOutlined } from "@ant-design/icons";
+import { CloseOutlined, LeftOutlined, RightOutlined, SearchOutlined } from "@ant-design/icons";
 import Categories from "./Categories";
 import { Link } from "react-router-dom";
 // import Requirement from "./Requirement";
@@ -17,9 +17,31 @@ import Enquiry from "./Enquiry";
 import { useState } from "react";
 
 import { useRef } from "react";
-import Modelcomponent from "./Modelcomponent";
+import axios from "axios";
+//import { Input } from "@material-tailwind/react";
+// import Modelcomponent from "./Modelcomponent";
 
 const Home = () => {
+    const [step, setStep] = useState(1);
+    const [mobile, setMobile] = useState('');
+    const [otp, setOtp] = useState('');
+    const [fdata, setFdata] = useState({ name: "", email: "", mobile: mobile, area: "" });
+    const handlerequest = async () => {
+        // return  setStep(step + 1);
+        const data = {
+            ...fdata
+        }
+        await axios.post("http://192.168.1.14:2130/send_email", data)
+            .then(res => {
+                setStep(step + 1);
+                console.log(res)
+            })
+    }
+    const handlefdata = (e) => {
+        const name = e.target.name;
+        const val = e.target.value;
+        setFdata(prev => ({ prev, [name]: val }));
+    }
     let sliderRef = useRef(null);
     const next = () => {
         sliderRef.slickNext();
@@ -43,6 +65,8 @@ const Home = () => {
         //nextArrow : <NextArrow/>,
         //prevArrow: <PrevArrow/>
     };
+    const [showMyModal, setShowMyModal] = useState(false)
+    //const handleOnClose = () => setShowMyModal(false)
     return (
         <>
             <section className="banner overflow-hidden">
@@ -63,15 +87,10 @@ const Home = () => {
                                                 </>
                                             ))
                                         }
-
-
                                     </div>
-                                    <div className="px-2 flex shadow-md shadow-blue-300 rounded-full border border-blue-800 items-center bg-[white]">
-
+                                    <div onClick={() => setShowMyModal(true)} className="px-2 flex shadow-md shadow-blue-300 rounded-full border border-blue-800 items-center bg-[white]">
                                         <input type="text" placeholder="Search Location" className="w-full px-4 py-2 rounded-full   text-lg outline-none" />
-                                       <Link to={'/'}>
-                                       <button className="bg-[#2f2e4bf2] px-4 py-2 lg:text-md text-sm text-nowrap rounded-full text-white"> <SearchOutlined className="pr-[5px]" />SEARCH</button>
-                                       </Link>
+                                        <button className="bg-[#2f2e4bf2] px-4 py-2 lg:text-md text-sm text-nowrap rounded-full text-white"> <SearchOutlined className="pr-[5px]" />SEARCH</button>
                                     </div>
                                 </div>
                             </div>
@@ -95,10 +114,10 @@ const Home = () => {
                                 </Slider>
                                 <div style={{ textAlign: "center" }}>
                                     <button className="button absolute top-[270px] left-0 border border-blue-800 rounded-full size-10 leading-5 bg-white sliderbutton" onClick={previous}>
-                                        <LeftOutlined/>
+                                        <LeftOutlined />
                                     </button>
                                     <button className="button absolute top-[270px] right-0  border border-blue-800 rounded-full size-10 leading-5 bg-white sliderbutton" onClick={next}>
-                                        <RightOutlined/>
+                                        <RightOutlined />
                                     </button>
                                 </div>
                             </div>
@@ -111,7 +130,84 @@ const Home = () => {
             <Actionbox />
             <Enquiry />
             <Dreamhome />
-            <Modelcomponent/>
+            {
+                showMyModal && (
+                    <>
+                        <div className="fixed z-10 inset-0 overflow-y-auto">
+
+                            <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                                <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                                    <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                                </div>
+
+                                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+                                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full relative z-0">
+                                    <div className="bg-[#d7decd] px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
+                                        <div className="w-full pb-5 border-b border-gray-200">
+                                            <img src={logo} alt='image' className='mx-auto h-[45px]' />
+                                            <button onClick={() => setShowMyModal(false)} className='bg-white   absolute top-[5px] right-[5px] rounded-full size-10 leading-1'><CloseOutlined className='text-md font-bold' /></button>
+                                        </div>
+                                        <div className="w-full">
+                                            {
+                                                step == 1 && (
+                                                    <>
+                                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                                            <h3 className="text-2xl leading-6 font-medium text-gray-900 mt-8">Enter your Phone number</h3>
+                                                            <div className="mt-3">
+                                                                <p className="text-md text-[#0d6efd] font-semibold">We wll send you a verification code on the same number.</p>
+                                                            </div>
+                                                            <div className="mt-3">
+                                                                <input type="text" placeholder="" value={mobile} onChange={(e) => setMobile(e.target.value)} className="rounded-full bg-white text-black w-full py-2 px-2 outline-none" />
+                                                            </div>
+                                                        </div>
+                                                    </>
+                                                )
+                                            }
+                                            {
+                                                step == 2 && (
+                                                    <>
+                                                     <div className="form-group w-full block">
+                                                            <label htmlFor="">Enter OTP</label>
+                                                            <input type="text" className="w-full p-2" value={otp} onChange={(e) => setOtp(e.target.value)} />
+                                                        </div>
+                                                    </>
+                                                )
+                                            }
+                                            {
+                                                step == 3 && (
+                                                    <>
+                                                        <div className="form-group w-full block">
+                                                            <label htmlFor="">Enter Name</label>
+                                                            <input type="text" className="w-full p-2" value={fdata?.name} onChange={handlefdata} />
+                                                        </div>
+                                                        <div className="form-group w-full block">
+                                                            <label htmlFor="">Email</label>
+                                                            <input type="text" className="w-full p-2" value={fdata?.email} onChange={handlefdata} />
+                                                        </div>
+                                                        <div className="form-group w-full block">
+                                                            <label htmlFor="">Area</label>
+                                                            <input type="text" className="w-full p-2" value={fdata?.area} onChange={handlefdata} />
+                                                        </div>
+                                                    </>
+                                                )
+                                            }
+
+
+                                        </div>
+                                    </div>
+                                    <div className="bg-[#d7decd] px-4 py-3 pb-6 sm:px-6 sm:flex">
+                                        <button onClick={handlerequest} className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-8 py-2 bg-[#0d6efd] text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2  sm:ml-3 sm:w-auto sm:text-sm">
+                                            NEXT
+                                        </button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
 
         </>
     )
